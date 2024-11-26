48628a02f6a87439e16ebe5f3348ad38fbd93d13
Listiananda Apriliawan <nanda-l.apriliawan@aia.com>
2024-11-26 19:02:51 +0800

    feat: add constants

---
+++ src/constants/colors.tsx
@@ -0,0 +1,9 @@
+export const colors = {
+  mainColor: '#142535',
+  white: '#FFFFFF',
+  textColor: '#4A5F73',
+  lightBlue: '#DBE9F5',
+  red: '#ff7979',
+  imageBackground: '#F6F8FB',
+  transparent: 'transparent',
+};
